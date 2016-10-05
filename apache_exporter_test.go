b8fb39e41681dfdedbfb9b36c2ecd4b3b49c1250
Ruslan Islamgaliev <neezgee@gmail.com>
2016-10-05 21:40:36 +0100

    Updated test (scoreboard metrics counted)

--- apache_exporter_test.go
+++ apache_exporter_test.go
@@ -99,9 +99,9 @@ IdleWorkers: 8
 Scoreboard: _W_______K......................................................................................................................................................................................................................................................
 `
 
-	metricCountApache22 = 6
-        metricCountApache24 = 10
-        metricCountApache24Worker = 6
+	metricCountApache22 = 10
+        metricCountApache24 = 12
+        metricCountApache24Worker = 10
 
 )
 
